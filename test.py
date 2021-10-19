dc280cbfe78dd2eb181b3c95a390a4bd4b232073
Ian Arellano <ian.g.arellano01@gmail.com>
2021-10-19 11:16:16 -0500

    adding test.py to practice python

---
+++ test.py
@@ -0,0 +1,56 @@
+
+# BASICS
+# a = 1
+# a = a + 1
+# b = a
+# print(a)
+#
+# print("hello" * 3)
+#
+# print(a == 2)
+#
+# empty = None
+# print(empty)
+#
+# # None()
+#
+# print(not a)
+#
+# print(1 == 1 and 2 == 2)
+
+# FUNCTIONS
+# user_number = input("Enter a number: ")
+# print(user_number)
+
+# print("Hello", "World")
+
+# IF ELSE AND ELIF
+
+its_raining = True
+# if its_raining:
+#     print("It's raining!")
+
+# its_raining = False
+# if its_raining:
+#     print("It's raining!")
+
+if its_raining:
+    print("It's raining!")
+else:
+    print("It's not raining.")
+
+print("Hello!")
+password = input("Enter the correct password: ")
+
+if password == "password":
+    print("Welcome, Sir.")
+else:
+    print("Wrong password.")
+
+print("Hello!")
+word = input("Enter something: ")
+
+if word == "hi":
+    print("Hi to you too!")
+elif word == "hello":
+    print("Hello hello!")
